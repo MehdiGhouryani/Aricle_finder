20df7b63c668fe9d54bb50a00cf7f3a0f8b7d252
Mehdi <mohammadmahdi670@gmail.com>
2025-01-08 22:56:38 +0330

    ai

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 from telegram import Bot
+import os
+
+
+TOKEN = os.getenv('TOKEN')
+bot = Bot(TOKEN)
 
 ADMIN_CHAT_ID=['1717599240']
 BOT_USERNAME = "MaghaleBazbot"
@@ -13,6 +18,6 @@ def reset_user_data(context):
 async def send_error_to_admin(error_message):
     admin_user_id = 1717599240
     try:
-       await Bot.send_message(chat_id=admin_user_id, text=f"ERROR : {str(error_message)}")
+       await bot.send_message(chat_id=admin_user_id, text=f"ERROR : {str(error_message)}")
     except Exception as e:
         print(f"Failed to send error message to admin: {str(e)}")
