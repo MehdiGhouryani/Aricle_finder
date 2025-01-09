80d8c6618d31c4b90ecf8bd3fde1371903d43a6a
Mehdi <mohammadmahdi670@gmail.com>
2025-01-09 10:57:08 +0330

    ai

--- handlers/AI.py
+++ handlers/AI.py
@@ -4,6 +4,11 @@ import google.generativeai as genai
 from telegram.constants import ParseMode
 import os
 from config import reset_user_data
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 
 gen_token =os.getenv("genai")
 
@@ -16,14 +21,14 @@ async def summarizing(update:Update,context:ContextTypes.DEFAULT_TYPE):
     reset_user_data(context)
 
     try:
-            response = model.generate_content(f"""
+        response = model.generate_content(f"""
 مقاله زیر رو بررسی کن و یک خلاصه خیلی کامل ازش بفرست واسم.
 خلاصه‌ای که مینویسی به زبان عامیانه و روان فارسی باشه.
 
 اگر مقاله واست در دسترس هم نیست . 
 فقط یک پیام کوتاه بنویس
-مثل مقاله در دسترس نیست یا openaccess نیست دوست عزیز.\n\n{article}""")
+مثل مقاله در دسترس نیست یا openaccess نیست دوست عزیز.\n\n{str(article)}""")
             
-            await update.message.reply_text(response.text,parse_mode=ParseMode.MARKDOWN)
+        await update.message.reply_text(response.text,parse_mode=ParseMode.MARKDOWN)
     except Exception as e:
         await context.bot.send_message(text=e,chat_id=1717599240)
